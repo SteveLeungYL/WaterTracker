65959469b83944899276fdebab50bfc585c08d1a
Yu Liang <luy70@psu.edu>
2024-10-30 18:36:21 -0400

    (feat) Updated the view to handle the change of unit.

--- Shared/InvisibleSlider.swift
+++ Shared/InvisibleSlider.swift
@@ -9,16 +9,22 @@ import SwiftUI
 
 struct InvisibleSlider: View {
     
-    @Binding var percent: Double
+    @Binding var drinkNum: Double
     @Binding var waveOffset: Angle
     
+    @Environment(\.modelContext) var modelContext
+    @State var cupMinimumNum: Double = 10.0
+    @State var cupCapacity: Double = 250.0
+    @State var adjustStep: Double = 10.0
+    @State var scroll: Double = 0.0
+
     var body: some View {
         GeometryReader { geo in
             #if !os(watchOS)
             let dragGesture = DragGesture(minimumDistance: 0)
                 .onChanged { value in
                     let percent = 1.0 - Double(value.location.y / geo.size.height)
-                    self.percent = max(5, min(100, percent * 100))
+                    self.drinkNum = max(self.cupMinimumNum, min(cupCapacity, percent * cupCapacity))
                 }
             #endif
             
@@ -33,22 +39,35 @@ struct InvisibleSlider: View {
             Text("For Digital Crown")
                 .opacity(0.000001)
                 .focusable()
-                .digitalCrownRotation(detent: $percent,
-                                      from: 5.0,
+                .digitalCrownRotation(detent: $scroll,
+                                      from: 0.0,
                                       through: 100.0,
-                                      by: 5.0,
+                                      by: 1.0,
                                       isContinuous: false
                 ) { crownEvent in
                 }
                 .scrollIndicators(.hidden)
 #endif
+        }.onAppear() {
+            let config = getWaterTracerConfiguration(modelContext: modelContext)
+            self.cupMinimumNum = getCupMinimumNum(config:config)
+            self.cupCapacity = getCupCapacity(config:config)
+            self.adjustStep = getUnitStep(config:config)
+            self.scroll = Double(drinkNum - cupMinimumNum) / adjustStep
         }
+#if os(watchOS)
+        .onChange(of: scroll) {
+            self.drinkNum = max(cupMinimumNum, min(cupCapacity, Double(scroll) * adjustStep))
+            if (scroll > ((cupCapacity - cupMinimumNum) / adjustStep) + 1) {
+                scroll = ((cupCapacity - cupMinimumNum) / adjustStep) + 1
+            }
+        }
+#endif
     }
-    
 }
 
 #Preview {
     @Previewable @State var percent: Double = 20
     @Previewable @State var waveOffset: Angle = Angle(degrees: 0.0)
-    InvisibleSlider(percent: $percent, waveOffset: $waveOffset)
+    InvisibleSlider(drinkNum: $percent, waveOffset: $waveOffset)
 }
--- Shared/WaveAnimation.swift
+++ Shared/WaveAnimation.swift
@@ -10,48 +10,56 @@ import SwiftUI
 
 struct WaveAnimation: View {
     
-    @Binding var percent: Double
+    @Binding var drinkNum: Double
     @Binding var waveOffset: Angle
     
-    init(_ percent: Binding<Double>, _ waveOffset: Binding<Angle>) {
-        self._percent = percent
+    init(_ drinkNum: Binding<Double>, _ waveOffset: Binding<Angle>) {
+        self._drinkNum = drinkNum
         self._waveOffset = waveOffset
     }
     
     var body: some View {
         ZStack{
-            WaveWithHeight(percent: $percent, waveOffset: $waveOffset)
+            WaveWithHeight(drinkNum: $drinkNum, waveOffset: $waveOffset)
         }
     }
 }
 
 struct WaveWithHeight: View {
-    @Binding var percent: Double
+    @Binding var drinkNum: Double
     @Binding var waveOffset: Angle
+    
+    @Environment(\.modelContext) var modelContext
+    @State var cupCapacity: Double = 1000.0
     
     var body : some View {
         GeometryReader { geometry in
             ZStack {
-                Wave(offSet: Angle(degrees: waveOffset.degrees + 270), percent: 100)
+                Wave(offSet: Angle(degrees: waveOffset.degrees + 270))
                     .fill(Color.blue)
                     .opacity(0.3)
                     .animation(.linear(duration: 2.3).repeatForever(autoreverses: false), value: waveOffset)
                 
-                Wave(offSet: Angle(degrees: waveOffset.degrees + 90), percent: 100)
+                Wave(offSet: Angle(degrees: waveOffset.degrees + 90))
                     .fill(Color.blue)
                     .opacity(0.4)
                     .animation(.linear(duration: 1.8).repeatForever(autoreverses: false), value: waveOffset)
                 
-                Wave(offSet: Angle(degrees: waveOffset.degrees), percent: 100)
+                Wave(offSet: Angle(degrees: waveOffset.degrees))
                     .fill(Color.blue)
                     .onAppear {
                         waveOffset = waveOffset + Angle(degrees: 360)
                     }
                     .animation(.linear(duration: 1.7).repeatForever(autoreverses: false), value: waveOffset)
             }
-            .animation(.linear(duration: 0.3), value: percent)
-            .offset(x:0, y: geometry.size.height * (1.0 - percent / 100.0))
+            .animation(.linear(duration: 0.3), value: drinkNum)
+            .offset(x:0, y: geometry.size.height * (1.0 - drinkNum / self.cupCapacity))
             .frame(width: geometry.size.width, height: geometry.size.height)
+        }.onAppear() {
+            let config = getWaterTracerConfiguration(modelContext: modelContext)
+            withAnimation(.linear(duration: 0.2)) {
+                self.cupCapacity = getCupCapacity(config:config)
+            }
         }
     }
 }
@@ -59,7 +67,7 @@ struct WaveWithHeight: View {
 struct Wave: Shape {
     
     var offSet: Angle
-    var percent: Double
+    var percent: Double = 100
     
     public mutating func set_offSet(_ offSet: Angle) {
         self.offSet = offSet
@@ -101,11 +109,11 @@ struct Wave: Shape {
 
 
 #Preview {
-    @Previewable @State var percent: Double = 20
+    @Previewable @State var drinkNum: Double = 100.0
     @Previewable @State var waveOffset: Angle = Angle(degrees: 0.0)
     ZStack {
-        WaveAnimation($percent, $waveOffset)
-        InvisibleSlider(percent: $percent, waveOffset: $waveOffset)
-        Text("\(percent)%")
+        WaveAnimation($drinkNum, $waveOffset)
+        InvisibleSlider(drinkNum: $drinkNum, waveOffset: $waveOffset)
+        Text("\(drinkNum)ml")
     }
 }
