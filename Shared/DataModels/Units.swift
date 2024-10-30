42fa402d1d92ed6611023abcefbf4c1c24a0a50f
Yu Liang <luy70@psu.edu>
2024-10-30 18:35:50 -0400

    (feat) Water Tracer unit saved in SwiftData.

---
+++ Shared/DataModels/Units.swift
@@ -0,0 +1,48 @@
+//
+//  Units.swift
+//  WaterTracer
+//
+//  Created by Yu Liang on 10/30/24.
+//
+
+enum WaterUnits: Codable, Hashable {
+    
+    case oz
+    case ml
+    
+    var cupDefaultCapacity: Double {
+        switch self {
+        case .oz:
+            return 8.5
+        case .ml:
+            return 250.0
+        }
+    }
+    
+    var cupMinimumNum: Double {
+        switch self {
+        case .oz:
+            return 0.1
+        case .ml:
+            return 10
+        }
+    }
+
+    var unitStep: Double {
+        switch self {
+        case .oz:
+            return 0.1
+        case .ml:
+            return 10
+        }
+    }
+    
+    var unitStr: String {
+        switch self {
+        case .oz:
+            return "oz"
+        case .ml:
+            return "ml"
+        }
+    }
+}
