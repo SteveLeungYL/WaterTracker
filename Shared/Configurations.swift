59f0f94e634f2901bd0635a560aafec0c3079fa4
Yu Liang <luy70@psu.edu>
2024-10-29 21:28:32 -0400

    (feat) Updates on the healthkit manager handling.

---
+++ Shared/Configurations.swift
@@ -0,0 +1,8 @@
+//
+//  Configurations.swift
+//  WaterTracer
+//
+//  Created by Yu Liang on 10/29/24.
+//
+
+let AppName = "Water Tracer"
--- Shared/HealthKitManager.swift
+++ Shared/HealthKitManager.swift
@@ -10,30 +10,44 @@ import HealthKit
 import WidgetKit
 
 class HealthKitManager: ObservableObject {
+    
     static let shared = HealthKitManager()
     var healthStore = HKHealthStore()
     
-    func requestAuthorization() {
-        // this is the type of data we will be reading from Health (e.g stepCount)
-        let toReads = Set([
-            HKObjectType.quantityType(forIdentifier: .dietaryWater)!])
-        
-        // this is to make sure User's Heath Data is Avaialble
+    // This is the type of data we will be reading from Health (e.g dietaryWater)
+    let toReadAndWrite = Set([
+        HKObjectType.quantityType(forIdentifier: .dietaryWater)!])
+    
+    func checkHealthKitAvailability() {
         guard HKHealthStore.isHealthDataAvailable() == true else {
-            print("health data not available!")
+            print("Error: HealthKit is not available on the current device.")
             return
         }
+    }
+    
+    func requestAuthorization() {
         
-        // asking User's permission for their Health Data
-        // note: toShare is set to nil since I'm not updating any data
-        healthStore.requestAuthorization(toShare: toReads, read: toReads) {
+        // This is to make sure device's Heath Data is Avaialble
+        checkHealthKitAvailability()
+        
+        // Asking User's permission for their Health Data
+        healthStore.requestAuthorization(toShare: toReadAndWrite, read: toReadAndWrite) {
             success, error in
-            if success {
-//                self.fetchAllDatas()
-            } else {
-                print("\(String(describing: error))")
+            if !success {
+                print("Cannot access HealthKit. \(AppName) relies sorely on HealthKit from Apple to save data and syncronize between devices. Please enable the HealthKit access in Settings. ")
             }
         }
+    }
+    
+    func saveDrinkWater(drink_num: Double) {
+        checkHealthKitAvailability()
+        // Request permission again if the user change the permission outside the app.
+        // OK if the permission is already granted. No repeated pop-up screen.
+        requestAuthorization()
+        
+        // TODO:: Save to HealthKit.
+        
+        return
     }
     
     init() {
