db138d6506d60fed644648ee609aa49e01993cb8
Yu Liang <luy70@psu.edu>
2024-10-29 22:04:48 -0400

    (feat) Alert for Healthkit error.

--- Shared/HealthKitManager.swift
+++ Shared/HealthKitManager.swift
@@ -18,39 +18,70 @@ class HealthKitManager: ObservableObject {
     let toReadAndWrite = Set([
         HKObjectType.quantityType(forIdentifier: .dietaryWater)!])
     
-    func checkHealthKitAvailability() {
+    func checkHealthKitAvailability() -> HealthKitError? {
         guard HKHealthStore.isHealthDataAvailable() == true else {
-            print("Error: HealthKit is not available on the current device.")
-            return
+            return .healthKitNotAvailable
         }
+        return nil
     }
     
-    func requestAuthorization() {
+    func requestAuthorization() -> HealthKitError? {
         
         // This is to make sure device's Heath Data is Avaialble
-        checkHealthKitAvailability()
+        if let err = checkHealthKitAvailability() {
+            return err
+        }
+        
+        var err: HealthKitError? = nil
         
         // Asking User's permission for their Health Data
         healthStore.requestAuthorization(toShare: toReadAndWrite, read: toReadAndWrite) {
             success, error in
             if !success {
-                print("Cannot access HealthKit. \(AppName) relies sorely on HealthKit from Apple to save data and syncronize between devices. Please enable the HealthKit access in Settings. ")
+                err = .healthKitNotAvailable
             }
         }
+        
+        return err
     }
     
-    func saveDrinkWater(drink_num: Double) {
-        checkHealthKitAvailability()
+    func saveDrinkWater(drink_num: Double) -> HealthKitError? {
+        if let errMsg = checkHealthKitAvailability() {
+            return errMsg
+        }
         // Request permission again if the user change the permission outside the app.
         // OK if the permission is already granted. No repeated pop-up screen.
-        requestAuthorization()
+        if let errMsg = requestAuthorization() {
+            return errMsg
+        }
         
         // TODO:: Save to HealthKit.
         
-        return
+        return nil
     }
     
     init() {
-        requestAuthorization()
+        // TODO:: This should not be here.
+        // The check should be in the introduction tab view process.
+        // FIXME:: Fix later.
+        _ = requestAuthorization()
     }
+}
+
+enum HealthKitError: LocalizedError {
+    case healthKitNotAvailable
+    
+    var errorDescription: String? {
+        switch self {
+        case .healthKitNotAvailable:
+          return "Can't access HealthKit. "
+        }
+      }
+
+      var recoverySuggestion: String? {
+        switch self {
+        case .healthKitNotAvailable:
+          return "\(AppName) relies sorely on HealthKit from Apple to save data and synchronize between devices. Without HealthKit, the water reminder still works, but no water tracking would be saved. \nTo enable full \(AppName) features, please enable the HealthKit access in Settings. "
+        }
+      }
 }
