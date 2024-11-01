6602a37eb9f887395909cec9371ce81f3d3de595
Yu Liang <luy70@psu.edu>
2024-11-01 17:01:45 -0400

    (feat) Sync notification between iOS and watchOS.

--- Shared/Notifications/NotificationHandler.swift
+++ Shared/Notifications/NotificationHandler.swift
@@ -6,8 +6,58 @@
 //
 
 import UserNotifications
+import WatchConnectivity
+import Foundation
 
-public struct NotificationHandler {
+final class CrossOsConnectivity: NSObject, ObservableObject {
+    
+    // Is published necessary?
+    @Published var receivedInfo: String = ""
+    
+    // For communicating between watchOS and iOS
+    static let shared = CrossOsConnectivity()
+    
+    private override init() {
+        super.init()
+        
+#if !os(watchOS)
+        guard WCSession.isSupported() else {
+            return
+        }
+#endif
+        WCSession.default.delegate = self
+        WCSession.default.activate()
+        
+    }
+    
+    public func sendNotificationReminder() {
+        
+        guard WCSession.default.activationState == .activated else {
+            return
+        }
+        
+        // 1
+#if os(watchOS)
+        // 2
+        guard WCSession.default.isCompanionAppInstalled else {
+            return
+        }
+#else
+        // 3
+        guard WCSession.default.isWatchAppInstalled else {
+            return
+        }
+#endif
+        
+        // Ignore the reponse handler.
+        // And ignore the error handler either.
+        // If the companion doesn't reponse, we cannot do anything. 🤷🏻
+        // Inconsistent notification it is. 🤷🏻
+        WCSession.default.sendMessage(["msg": "Send Reminder"], replyHandler: nil)
+    }
+}
+
+public final class LocalNotificationHandler {
     
     static public func askForNotificationAuthorization() async {
         
@@ -22,25 +72,55 @@ public struct NotificationHandler {
     
     static public func registerLocalNotification() {
         
-        // FIXME:: Should not be here.
         Task {
-            await NotificationHandler.askForNotificationAuthorization()
+            await LocalNotificationHandler.askForNotificationAuthorization()
         }
-
+        
         // Configure the notification's payload.
         let content = UNMutableNotificationContent()
         content.title = NSString.localizedUserNotificationString(forKey: "Time to drink water!", arguments: nil)
         content.body = NSString.localizedUserNotificationString(forKey: "Log your water status in \(AppName).", arguments: nil)
         content.sound = UNNotificationSound.default
-         
+        
         // Deliver the notification in five seconds.
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 7200, repeats: false) //  2 hours.
         let request = UNNotificationRequest(identifier: "SteveLeung.WaterTracer.DeferredNotification", content: content, trigger: trigger) // Schedule the notification.
         let center = UNUserNotificationCenter.current()
         center.add(request) { (error : Error?) in
-             if let theError = error {
-                 print(theError.localizedDescription)
-             }
+            if let theError = error {
+                print(theError.localizedDescription)
+            }
         }
     }
+}
+
+// MARK: - WCSessionDelegate
+extension CrossOsConnectivity: WCSessionDelegate {
+#if os(iOS)
+    func sessionDidBecomeInactive(_ session: WCSession) {
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        WCSession.default.activate()
+    }
+#endif
+    
+    func session(
+        _ session: WCSession,
+        activationDidCompleteWith activationState: WCSessionActivationState,
+        error: Error?
+    ) {
+    }
+    
+    // This method is called when a message is sent with failable priority
+    // and a reply was *not* requested.
+    func session(
+        _ session: WCSession,
+        didReceiveMessage message: [String: Any]
+    ) {
+        // Just register the information.
+        // And that's it.
+        LocalNotificationHandler.registerLocalNotification()
+    }
+    
 }
