cb174cf30f1295bf0f7dae8710fc32aaf1423819
Yu Liang <luy70@psu.edu>
2024-11-04 16:25:29 -0500

    (feat) Add target for iOS widgetkit.

---
+++ WaterTracer Widget/WaterTracer_WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WaterTracer_WidgetBundle.swift
+//  WaterTracer Widget
+//
+//  Created by Yu Liang on 11/4/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WaterTracer_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WaterTracer_Widget()
+    }
+}
