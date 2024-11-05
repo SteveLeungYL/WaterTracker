6d3eeb941600f5bc5ef9ed855d9d3a7464e9e5d4
Yu Liang <luy70@psu.edu>
2024-11-05 13:58:57 -0500

    (feat) Appintent for changing day view or week view.

--- WaterTracer Watch Widget/AppIntent.swift
+++ WaterTracer Watch Widget/AppIntent.swift
@@ -8,11 +8,24 @@
 import WidgetKit
 import AppIntents
 
+enum WidgetDataRange: String, AppEnum {
+    
+    static var typeDisplayRepresentation: TypeDisplayRepresentation = "Data Range"
+    
+    static var caseDisplayRepresentations: [WidgetDataRange : DisplayRepresentation] = [
+        .hour: DisplayRepresentation(stringLiteral: "Hour View"),
+        .week: DisplayRepresentation(stringLiteral: "Week View"),
+    ]
+    
+    case hour = "Hour View"
+    case week = "Week View"
+}
+
 struct ConfigurationAppIntent: WidgetConfigurationIntent {
-    static var title: LocalizedStringResource { "Dummy App Intent" }
-    static var description: IntentDescription { "Stay tuned for the App Intent feature. " }
+    static var title: LocalizedStringResource { "Data Range" }
+    static var description: IntentDescription { "Showing 24 hour view or week view" }
 
     // An example configurable parameter.
-    @Parameter(title: "Stay tuned for the App Intent", default: "😃")
-    var favoriteEmoji: String
+    @Parameter(title: "Choose view", default: .hour)
+    var chosenView: WidgetDataRange
 }
---
+++ WaterTracer Watch Widget/WaterTracer_Watch_Widget_Bundle.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTracer_Watch_Widget_Bundle.swift
+//  WaterTracer
+//
+//  Created by Yu Liang on 11/5/24.
+//
+
+import WidgetKit
+import SwiftUI
+import SwiftData
+
+@main
+struct WaterTracer_Watch_Widget_Bundle: WidgetBundle {
+    var body: some Widget {
+        WaterTracer_Watch_Widget(isDayView: true)
+    }
+}
