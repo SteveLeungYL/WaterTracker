73da235166ee165ca8e1b9a6063079cd997b7e6e
Yu Liang <yuliangdoctor@gmail.com>
2024-12-03 16:27:48 -0500

    (refactor) Clean up for the BarChart view.

--- Shared/Charts/BarChart.swift
+++ Shared/Charts/BarChart.swift
@@ -32,14 +32,6 @@ struct WaterTracingBarChart: View {
     //    @State private var rawSelectedDate: Date?
     @State var config: WaterTrackerConfigManager
     
-    //    var selectedData: HealthMetric? {
-    //        guard let rawSelectedDate else { return nil }
-    //
-    //        return chartData.first {
-    //            Calendar.current.isDate(rawSelectedDate, inSameDayAs: $0.date)
-    //        }
-    //    }
-    
     var body: some View {
         VStack { // Overall chart card
             #if !WIDGET && !WATCH_WIDGET
@@ -91,16 +83,6 @@ struct WaterTracingBarChart: View {
             #endif
             
             Chart {
-                //                if let selectedData {
-                //                    RuleMark(x: .value("Selected Data", selectedData.date, unit: .day))
-                //                        .foregroundStyle(Color.secondary.opacity(0.4))
-                //                        .offset(y: -12)
-                //                        .annotation(position: .top,
-                //                                    spacing: 0,
-                //                                    overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) {
-                //                            annotationView
-                //                        }
-                //                }
                 if self.dateComponents == .day {
                     ForEach(chartData) { curDateTracker in
                         BarMark(x: .value("Day", curDateTracker.date, unit: .day),
@@ -120,7 +102,6 @@ struct WaterTracingBarChart: View {
             #if !WIDGET && !WATCH_WIDGET
             .frame(height: 150)
             #endif
-            //            .chartXSelection(value: $rawSelectedDate.animation(.easeIn))
             .chartXAxis {
                 if self.dateComponents == .day {
                     #if !os(watchOS)
@@ -165,24 +146,6 @@ struct WaterTracingBarChart: View {
         .background(RoundedRectangle(cornerRadius: 12).fill(.regularMaterial))
         #endif
     }
-    
-    //    var annotationView: some View {
-    //        VStack(alignment: .leading) {
-    //            Text(selectedData?.date ?? .now, format: .dateTime.weekday(.abbreviated).day().month(.abbreviated))
-    //                .font(.footnote.bold())
-    //                .foregroundStyle(.secondary)
-    //
-    //            Text(selectedData?.value ?? 0, format: .number.precision(.fractionLength(2)))
-    //                .fontWeight(.heavy)
-    //                .foregroundStyle(.blue.gradient)
-    //        }
-    //        .padding(12)
-    //        .background(
-    //            RoundedRectangle(cornerRadius: 4)
-    //                .fill(.regularMaterial)
-    //                .shadow(color: .secondary.opacity(0.3), radius: 2, x: 2, y: 2)
-    //        )
-    //    }
 }
 
 #Preview {
@@ -191,6 +154,22 @@ struct WaterTracingBarChart: View {
     
     // Random week data for Preview. 
     @Previewable @State var mockChartData: [HealthMetric] = fillEmptyData(drinkDataRaw: [], startDate: NSCalendar.current.date(byAdding: .day, value: -7, to: getStartOfDate(date: Date()))!, endDate:getStartOfDate(date: Date()), gapUnit: .day, isMock: true)
+    
+    ZStack {
+        WaterTracingBarChart(chartData: mockChartData, dateComponents: .day, mainTitle: "Week Tracker", subTitle: "Showing last 7 days data", config: WaterTrackerConfigManager())
+            .modelContainer(sharedWaterTrackerModelContainer)
+            .environment(healthKitManager)
+            .environment(configManager)
+    }
+}
+
+
+#Preview {
+    @Previewable @State var healthKitManager = HealthKitManager()
+    @Previewable @State var configManager = WaterTrackerConfigManager()
+    
+    // Random week data for Preview.
+    @Previewable @State var mockChartData: [HealthMetric] = fillEmptyData(drinkDataRaw: [], startDate: NSCalendar.current.date(byAdding: .day, value: -7, to: getStartOfDate(date: Date()))!, endDate:getStartOfDate(date: Date()), gapUnit: .day, isMock: false)
     
     ZStack {
         WaterTracingBarChart(chartData: mockChartData, dateComponents: .day, mainTitle: "Week Tracker", subTitle: "Showing last 7 days data", config: WaterTrackerConfigManager())
